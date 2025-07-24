5dc01606ebb99f59b3fd5f6d36ea257ba758b888
K0d3x 8its <k0d3x.pm.me>
2025-07-24 18:43:47 -0400

    refactored code to separate the toggle and move to utils

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,11 +1,14 @@
--- toggleterm config & persistent “dev” terminal
 return {
   "akinsho/toggleterm.nvim",
   version = "*",
   keys = {
-    { "<C-x>", function()
-        require("plugins.toggleterm").toggle_dev()
-      end, desc = "Toggle Dev Terminal" },
+    {
+      "<C-x>",
+      function()
+        require("utils.term_toggle").toggle_dev()
+      end,
+      desc = "Toggle Dev Terminal",
+    },
   },
   config = function()
     require("toggleterm").setup {
@@ -15,24 +18,6 @@ return {
       start_in_insert = true,
       close_on_exit   = false,
     }
-
-    local Terminal = require("toggleterm.terminal").Terminal
-    local dev_term = Terminal:new {
-      cmd             = vim.o.shell,
-      dir             = vim.fn.expand("$HOME/dev"),
-      direction       = "horizontal",
-      size            = 10,
-      start_in_insert = true,
-      close_on_exit   = false,
-      hidden          = true,
-    }
-
-    local mod = {}
-    function mod.toggle_dev()
-      dev_term:toggle()
-    end
-
-    -- Export
-    package.loaded["plugins.toggleterm"] = mod
   end,
 }
+
