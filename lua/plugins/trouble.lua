68f54b6b9f85036257d8ec418f4403e5d6e95070
K0d3x 8its <k0d3x.pm.me>
2025-07-23 20:50:16 -0400

    added trouble.lua

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,13 @@
+-- lua/plugins/trouble.lua
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- optional but recommended
+  config = function()
+    require("trouble").setup({
+      icons = true,
+      use_diagnostic_signs = true, -- uses signs defined by LSP or :sign define
+    })
+  end,
+  cmd = "TroubleToggle", -- lazy-load on command
+}
+
