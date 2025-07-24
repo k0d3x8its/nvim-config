292edc52454b6e3c37e0ef93d59aa4f4137858aa
K0d3x 8its <k0d3x.pm.me>
2025-07-24 18:10:28 -0400

    refactored nvim-tree and the customer toggle option

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,31 +1,21 @@
 -- nvim-tree config & first-open toggle
 return {
-  "kyazdani42/nvim-tree.lua",
+  "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { "<C-s>", function()
-        require("plugins.nvim-tree").toggle_tree_at_dev()
-      end, desc = "Toggle File Tree at ~/dev" },
+    {
+      "<C-s>",
+      function()
+        require("utils.tree_toggle").toggle_at_dev()
+      end,
+      desc = "Toggle File Tree at ~/dev",
+    },
   },
   config = function()
-    local api = require("nvim-tree.api")
-    local mod = { first = true }
-
     require("nvim-tree").setup {
-      view = { side = "left", width = 28 },
+      view = { side = "left", width = 27 },
       update_focused_file = { enable = true },
     }
-
-    function mod.toggle_tree_at_dev()
-      if mod.first then
-        mod.first = false
-        api.tree.open({ path = vim.fn.expand("~/dev") })
-      else
-        api.tree.toggle()
-      end
-    end
-
-    -- Export toggle function
-    package.loaded["plugins.nvim-tree"] = mod
   end,
 }
+
