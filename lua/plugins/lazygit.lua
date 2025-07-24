a1924106a9a4313e7137e52fc190d6b62fd1901c
K0d3x 8its <k0d3x.pm.me>
2025-07-23 22:05:41 -0400

    added lazygit.lua

---
+++ lua/plugins/lazygit.lua
@@ -0,0 +1,12 @@
+-- ~/.config/nvim/lua/plugins/lazygit.lua
+return {
+  'kdheepak/lazygit.nvim',                   -- the Neovim wrapper for LazyGit
+  dependencies = { 'nvim-lua/plenary.nvim' },-- required helper lib
+  config = function()
+    -- Map <leader>gg to open LazyGit
+    vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', {
+      desc = ' Open LazyGit',
+      noremap = true, silent = true,
+    })
+  end,
+}
