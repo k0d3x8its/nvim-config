1ea20ee9327a8e32877ddecd2364f37c391b5868
K0d3x 8its <k0d3x.pm.me>
2025-07-30 21:16:16 -0400

    changed back to leader key

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,12 +1,16 @@
 -- ~/.config/nvim/lua/plugins/lazygit.lua
 return {
-  'kdheepak/lazygit.nvim',                   -- the Neovim wrapper for LazyGit
-  dependencies = { 'nvim-lua/plenary.nvim' },-- required helper lib
+  'kdheepak/lazygit.nvim',
+  event = "InsertEnter",-- the Neovim wrapper for LazyGit
+
+  dependencies = { 'nvim-lua/plenary.nvim' }, -- required helper lib
+  
   config = function()
     -- Map <leader>gg to open LazyGit
-    vim.keymap.set('n', '<C-g>g', '<cmd>LazyGit<CR>', {
+    vim.keymap.set('n', '<leader>gg', '<cmd>LazyGit<CR>', {
       desc = ' Open LazyGit',
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     })
   end,
 }
