8c50f187c90ad8f1efef46af2f02b7f6289b5877
K0d3x 8its <k0d3x.pm.me>
2025-08-03 15:16:01 -0400

    removed the pre buffer setup

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,39 +1,38 @@
-return {
-  -- Main completion plugin
-  {
-    "hrsh7th/nvim-cmp",
-    event = { "BufReadPre", "InsertEnter" },
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",         -- LSP source
-      "hrsh7th/cmp-buffer",           -- Buffer source
-      "hrsh7th/cmp-path",             -- Path source
-      "hrsh7th/cmp-cmdline",          -- Command-line source
-      "L3MON4D3/LuaSnip",             -- Snippet engine
-      "saadparwaiz1/cmp_luasnip",     -- LuaSnip source
-      "rafamadriz/friendly-snippets", -- Community source
-    },
-    config = function()
-      local cmp = require("cmp")
-      local luasnip = require("luasnip")
+-- main completion plugin
 
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<Tab>"] = cmp.mapping.select_next_item(),
-          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          { name = "buffer" },
-          { name = "path" },
-        }),
-      })
-    end,
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",         -- LSP source
+    "hrsh7th/cmp-buffer",           -- Buffer source
+    "hrsh7th/cmp-path",             -- Path source
+    "hrsh7th/cmp-cmdline",          -- Command-line source
+    "L3MON4D3/LuaSnip",             -- Snippet engine
+    "saadparwaiz1/cmp_luasnip",     -- LuaSnip source
+    "rafamadriz/friendly-snippets", -- Community source
   },
+  config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      }),
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+      }),
+    })
+  end,
 }
