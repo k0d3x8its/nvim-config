0eb506733176f3f0b467b8b60250c85cb4400e3d
K0d3x 8its <k0d3x.pm.me>
2025-08-03 16:44:28 -0400

    refactored code

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,18 +3,22 @@ return {
   "nvim-telescope/telescope.nvim",
   version = "0.1.x",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
+
   cmd = "Telescope",
+
   config = function()
-    require("telescope").setup {
+    local telescope = require("telescope")
+
+    telescope.setup {
       defaults = {
         vimgrep_arguments = {
           "rg", "--color=never", "--no-heading",
           "--with-filename", "--line-number",
           "--column", "--smart-case"
         },
-        prompt_prefix   = "🔍 ",
-        selection_caret = "➤ ",
-        path_display    = { "smart" },
+        prompt_prefix     = "🔍 ",
+        selection_caret   = "➤ ",
+        path_display      = { "smart" },
       },
     }
   end,
