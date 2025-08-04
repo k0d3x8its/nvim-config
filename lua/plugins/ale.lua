a84716344a668f0523ba63ab3193307bdb484224
K0d3x 8its <k0d3x.pm.me>
2025-08-04 14:21:13 -0400

    added various languages

--- lua/plugins/ale.lua
+++ lua/plugins/ale.lua
@@ -2,37 +2,44 @@
 
 return {
   "dense-analysis/ale",
-  ft = {
-    "python", "c", "cpp", "javascript", "typescript",
-    "html", "css", "solidity"
-  },
+
   config = function()
-    vim.g.ale_linters = {
-      python     = { 'flake8', 'mypy' },
-      c          = { 'gcc', 'cppcheck', 'arduino' },
-      cpp        = { 'g++', 'cppcheck', 'arduino' },
+    vim.g.ale_linters              = {
+      python = { 'flake8', 'mypy' },
+      c = { 'gcc', 'cppcheck', 'arduino' },
+      cpp = { 'g++', 'cppcheck', 'arduino' },
       javascript = { 'eslint' },
       typescript = { 'eslint' },
-      html       = { 'htmllint' },
-      css        = { 'stylelint' },
-      solidity   = { 'solhint' },
+      javascriptreact = { "eslint" },
+      typescriptreact = { "eslint" },
+      sh = { "shellcheck" },
+      json = { "jq" },
+      lua = { "luacheck" },
+      html = { 'htmllint' },
+      css = { 'stylelint' },
+      solidity = { 'solhint' },
     }
 
-    vim.g.ale_fix_on_save = 1
+    vim.g.ale_fix_on_save          = 1
 
-    vim.g.ale_fixers = {
-      ['*']        = { 'remove_trailing_lines', 'trim_whitespace' },
-      python       = { 'autopep8' },
-      javascript   = { 'prettier' },
-      typescript   = { 'prettier' },
-      html         = { 'prettier' },
-      css          = { 'prettier' },
+    vim.g.ale_fixers               = {
+      ['*']           = { 'remove_trailing_lines', 'trim_whitespace' },
+      python          = { 'autopep8' },
+      lua             = { "stylua" },
+      sh              = { "shfmt" },
+      javascript      = { 'prettier' },
+      typescript      = { 'prettier' },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      html            = { 'prettier' },
+      css             = { 'prettier' },
+      json            = { "prettier" },
     }
 
     vim.g.ale_lint_on_text_changed = 'always'
     vim.g.ale_lint_on_save         = 1
 
-    vim.g.ale_arduino_executable = 'arduino-cli'
-    vim.g.ale_arduino_fqbn       = 'arduino:avr:uno'
+    vim.g.ale_arduino_executable   = 'arduino-cli'
+    vim.g.ale_arduino_fqbn         = 'arduino:avr:uno'
   end,
 }
