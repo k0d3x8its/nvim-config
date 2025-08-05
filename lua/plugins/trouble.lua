180ff6ef0a1df529cf3fb44417c2422decda7ced
K0d3x 8its <k0d3x.pm.me>
2025-08-04 23:47:30 -0400

    refactored code

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,7 +3,9 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
-    require("trouble").setup({
+    local trouble = require("trouble")
+
+    trouble.setup({
       icons = true,
       use_diagnostic_signs = true, -- uses signs defined by LSP or :sign define
     })
