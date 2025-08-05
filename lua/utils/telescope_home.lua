832c096fb55a3325d858b17d1bf27aab446ae3c9
K0d3x 8its <k0d3x.pm.me>
2025-08-05 13:56:57 -0400

    added ability to access hidden files in telescope

--- lua/utils/telescope_home.lua
+++ lua/utils/telescope_home.lua
@@ -3,11 +3,11 @@
 local mod = {}
 
 function mod.find_files()
-  require('telescope.builtin').find_files({ cwd = vim.env.HOME })
+  require('telescope.builtin').find_files({ cwd = vim.env.HOME, hidden = true, })
 end
 
 function mod.live_grep()
-  require('telescope.builtin').live_grep({ cwd = vim.env.HOME })
+  require('telescope.builtin').live_grep({ cwd = vim.env.HOME, hidden = true, })
 end
 
 return mod
