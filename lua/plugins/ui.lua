9881ea8f6ac872a262d26c1ea100b99dd766a1b0
K0d3x 8its <k0d3x.pm.me>
2025-08-04 22:14:19 -0400

    added update status and icon in the statusline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -20,12 +20,32 @@ return {
     lazy         = false,
     config       = function()
       local lualine = require("lualine")
+      local lazy_status = require("lazy.status")
+
+      local function lazy_updates_icon_only()
+        local updates = lazy_status.updates()
+        if updates == "" then return "" end
+        local count = updates:match("(%d+)")
+        return count .. " "
+      end
 
       lualine.setup({
         options = {
           theme                = "dracula",
           section_separators   = { left = "", right = "" },
           component_separators = { left = " ", right = "" },
+        },
+        sections = {
+          lualine_x = {
+            {
+              lazy_updates_icon_only,
+              cond = lazy_status.has_updates,
+              color = { fg = "#ff9e64" },
+            },
+            "encoding",
+            "fileformat",
+            "filetype",
+          },
         },
       })
     end,
