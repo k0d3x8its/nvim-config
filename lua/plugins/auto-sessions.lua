1ff3d81269b81dfc0c963599dc2fd350e44d9d04
K0d3x 8its <k0d3x.pm.me>
2025-08-10 18:13:37 -0400

    fixed depreciation

--- lua/plugins/auto-sessions.lua
+++ lua/plugins/auto-sessions.lua
@@ -1,31 +1,22 @@
 return {
-  "rmagatti/auto-session",
-  event = "VimEnter",
+	"rmagatti/auto-session",
+	event = "VimEnter",
+	lazy = false,
 
-  config = function()
-    local auto_session = require("auto-session")
+	config = function()
+		local auto_session = require("auto-session")
 
-    auto_session.setup({
-      auto_restore_enabled = false,
-      auto_session_suppress_dirs = { "~/", "~/pictures/" },
-    })
+		auto_session.setup({
+			auto_restore = false,
+			suppresse_dirs = { "~/", "~/pictures/" },
+		})
 
-    local keymap = vim.keymap
+		local keymap = vim.keymap
 
-    -- restore last workspace session for current directory
-    keymap.set(
-      "n",
-      "<leader>wr",
-      "<cmd>SessionRestore<CR>",
-      { desc = "Restore session for cwd" }
-    )
+		-- restore last workspace session for current directory
+		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
 
-    -- save workspace session for current working directory
-    keymap.set(
-      "n",
-      "<leader>ws",
-      "<cmd>SessionSave<CR>",
-      { desc = "Save session for auto session root dir" }
-    )
-  end,
+		-- save workspace session for current working directory
+		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
+	end,
 }
