30b636c0255f081600774c9404c51068b0088d88
K0d3x 8its <k0d3x.pm.me>
2025-08-10 18:05:52 -0400

    disabled Perl & Ruby - based on a health check

--- init.lua
+++ init.lua
@@ -1,18 +1,24 @@
 -- 1️⃣ Bootstrap lazy.nvim
 local fn = vim.fn
-local lazypath = fn.stdpath("data").."/lazy/lazy.nvim"
+local lazypath = fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  fn.system({
-    "git","clone","--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
+	fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- 2️⃣ Python provider
-vim.g.python3_host_prog = '/home/broke/.config/nvim/venv/bin/python'
+vim.g.python3_host_prog = "/home/broke/.config/nvim/venv/bin/python"
+
+-- disabled Perl and Ruby
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
 
 -- 3️⃣ Hand off to unit.lua
 require("unit")
-
