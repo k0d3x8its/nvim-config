6a6b936127b3b57ea39dc6a908dbdcc07d19d307
K0d3x 8its <k0d3x.pm.me>
2025-08-11 15:00:23 -0400

    added live-preview for HTML/Markdown

---
+++ lua/plugins/live-preview.lua
@@ -0,0 +1,26 @@
+return {
+	"brianhuster/live-preview.nvim",
+	ft = { "markdown", "asciidoc", "html", "svg" },
+
+	dependencies = { "nvim-telescope/telescope.nvim" },
+
+	config = function()
+		local preview = require("livepreview.config")
+
+		preview.set({})
+
+		vim.o.autowriteall = true
+
+		local aug = vim.api.nvim_create_augroup("LivePreviewHTMLAutosave", { clear = true })
+		vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI", "InsertLeave" }, {
+			group = aug,
+			pattern = "*.html",
+			callback = function(args)
+				if vim.api.nvim_buf_is_valid(args.buf) and vim.bo[args.buf].modified then
+					vim.cmd([[silent! update]])
+				end
+			end,
+			desc = "Auto-save HTML so live-preview refreshes",
+		})
+	end,
+}
