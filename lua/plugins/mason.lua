b8e7a14484c122bd718f1c5d7b1393a76e0c0d86
K0d3x 8its <k0d3x.pm.me>
2025-08-16 22:39:35 -0400

    installed YAML with Mason

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,39 +1,39 @@
 -- Mason installer for LSP servers
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-  },
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+	},
 
-  lazy = false,
+	lazy = false,
 
-  config = function()
-    local mason = require("mason")
-    local mason_lspconfig = require("mason-lspconfig")
+	config = function()
+		local mason = require("mason")
+		local mason_lspconfig = require("mason-lspconfig")
 
-    mason.setup({
-      ui = {
-        package_installed = " ",
-        package_pending = " ",
-        package_uninstalled = " "
-      }
-    })
+		mason.setup({
+			ui = {
+				package_installed = " ",
+				package_pending = " ",
+				package_uninstalled = " ",
+			},
+		})
 
-    mason_lspconfig.setup({
-      ensure_installed = {
-        "pyright",
-        "ts_ls",
-        "html",
-        "cssls",
-        "lua_ls",
-        "solidity",
-        "bashls",
-        "jsonls",
-        "arduino_language_server"
-      },
-      automatic_installation = true,
-      automatic_enable = false,
-    })
-
-  end,
+		mason_lspconfig.setup({
+			ensure_installed = {
+				"pyright",
+				"ts_ls",
+				"html",
+				"cssls",
+				"lua_ls",
+				"solidity",
+				"bashls",
+				"jsonls",
+				"arduino_language_server",
+				"yamlls",
+			},
+			automatic_installation = true,
+			automatic_enable = false,
+		})
+	end,
 }
