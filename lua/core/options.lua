7bbee7042ec76efd9ae8f55e4a4adc68cda000d9
K0d3x 8its <k0d3x.pm.me>
2025-08-19 14:41:34 -0400

    fixed logical erorr for auto-session to occur properly

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,33 +1,40 @@
 -- UI & editing settings
-local opt = vim.opt           -- shorthand for vim.opt[...]
+local opt = vim.opt -- shorthand for vim.opt[...]
+
+-- sessions (needed for auto-session to restore highlights/locals)
+if not vim.tbl_contains(opt.sessionoptions:get(), "localoptions") then
+	opt.sessionoptions:append("localoptions")
+end
 
 -- statusline & tabs
-opt.laststatus  = 2           -- always show statusline
-opt.showtabline = 2           -- always show tabline
+opt.laststatus = 2 -- always show statusline
+opt.showtabline = 2 -- always show tabline
 
 -- line numbers & cursor
-opt.number      = true        -- show absolute line numbers
-opt.cursorline  = true        -- highlight current line
+opt.number = true -- show absolute line numbers
+opt.cursorline = true -- highlight current line
 
 -- tabs & indentation
-opt.tabstop     = 2           -- a <Tab> equals 2 spaces
-opt.shiftwidth  = 2           -- auto-indent to 2 spaces
-opt.softtabstop = 2           -- make tab key insert 2 spaces
-opt.expandtab   = true        -- convert tabs to spaces
-opt.smartindent = true        -- smart auto-indent
-opt.autoindent  = true        -- indent new lines like previous
+opt.tabstop = 2 -- a <Tab> equals 2 spaces
+opt.shiftwidth = 2 -- auto-indent to 2 spaces
+opt.softtabstop = 2 -- make tab key insert 2 spaces
+opt.expandtab = true -- convert tabs to spaces
+opt.smartindent = true -- smart auto-indent
+opt.autoindent = true -- indent new lines like previous
 
 -- wrapping & formatting
-opt.wrap        = true        -- wrap long lines
-opt.linebreak   = true        -- wrap at word boundaries
-opt.breakindent = true        -- maintain indent on wrapped lines
-opt.showbreak   = '↪ '        -- prepend continue lines with ↪
-opt.textwidth   = 0           -- disable hard wrapping
-opt.formatoptions:remove('t') -- don't auto-wrap text
-opt.splitright = true     -- vertical split placed to the right
+opt.wrap = true -- wrap long lines
+opt.linebreak = true -- wrap at word boundaries
+opt.breakindent = true -- maintain indent on wrapped lines
+opt.showbreak = "↪ " -- prepend continue lines with ↪
+opt.textwidth = 0 -- disable hard wrapping
+opt.formatoptions:remove("t") -- don't auto-wrap text
+opt.splitright = true -- vertical split placed to the right
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    vim.cmd("cd ~/dev")
-  end,
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.cmd("cd ~/dev")
+		end
+	end,
 })
